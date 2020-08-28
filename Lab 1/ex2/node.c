c09a9879d3421017f0614d3344b8d1be5d4ce8c5
noelmathewisaac <noelmathewisaac@gmail.com>
2020-08-28 22:54:36 +0800

    Free memory

--- Lab 1/ex2/node.c
+++ Lab 1/ex2/node.c
@@ -69,11 +69,13 @@ void insert_node_from_head_at(list *lst, int index, int data)
         {
             // printf("beg");
             insert_at_beginning(lst, data);
+            free(new_node);
         }
         else if (head->next == NULL)
         {
             // printf("end");
             insert_at_end(lst, data);
+            free(new_node);
         }
         else
         {
@@ -120,11 +122,13 @@ void insert_node_from_tail_at(list *lst, int index, int data)
         {
             // printf("end");
             insert_at_end(lst, data);
+            free(new_node);
         }
         else if (tail->prev == NULL)
         {
             // printf("beg");
             insert_at_beginning(lst, data);
+            free(new_node);
         }
         else
         {
@@ -208,7 +212,6 @@ void reset_list(list *lst)
 {
     lst->head = NULL;
     lst->tail = NULL;
-    free(lst);
 }
 
 // traverses list and applies func on data values of all elements in the list
